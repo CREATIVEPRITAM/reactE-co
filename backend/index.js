8d6441fc0f25015d0cfe8a8761a12d39c84e5c43
Pritam Singh <iamcreativepritam@gmail.com>
2024-02-16 23:55:14 +0530

    commite for backend

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,31 @@
 const port = 4000;
-const express = require('express');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors())
+
+
+//Data base connecto with mongoDB
+mongoose.connect("mongodb+srv://pritamsingh:Pritam@2023!@cluster0.a4djlbn.mongodb.net/reactE-com");
+
+
+// API creation
+
+app.get("/", (req, res)=> {
+    res.send("Express App is Running")
+})
+
+
+app.listen(port,(error) => {
+if(!error) {
+    console.log("Server Running on Port" +port)
+}else{
+    console.log("Error : "+error);
+}
+})
