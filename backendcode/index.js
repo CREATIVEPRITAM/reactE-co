bccd8734242225c64cb25f7e723c9deec7292326
Pritam Singh <iamcreativepritam@gmail.com>
2024-02-20 22:31:57 +0530

    making footer responsive

--- backendcode/index.js
+++ backendcode/index.js
@@ -1,4 +1,4 @@
-const port = 4000;
+const port = 3500;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -12,15 +12,36 @@ app.use(express.json());
 app.use(cors());
 
 // Connect the data base mongoDb Atlas
-mongoose.connect("mongodb+srv://pritamsingh:Pritam@2023!@cluster0.rmpucsd.mongodb.net/reactE-com");
-// mongoose.connect("mongodb+srv://pritamsingh:Pritam@2023!@cluster0.rmpucsd.mongodb.net/");
+// mongoose.connect("mongodb+srclv://pritamsingh:Pritam@2023!@cluster0.fov5dyq.mongodb.net/reactE-com");
 
-
-// API end oint creation;
+// API end point creation;
 
 app.get("/", (req, res) => {
-    res.send("Express Is Running")
+    res.send("Express App Is Running");
 })
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: "./upload",
+    filename : (req,file,cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+
+const upload = multer({storage: storage})
+
+// Creating upload Endpoint for imgae
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload", upload.single('product'),(req,res)=>{
+    res.json({
+        success: 1,
+        image_url: `http:localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
 
 app.listen(port, (error)=>{
     if(!error){
